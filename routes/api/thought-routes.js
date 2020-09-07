20b0b862f843864875c2279248a562142131f4a3
Florhaidee <florhaideedev@gmail.com>
2020-09-06 23:46:33 -0600

    created routes for thought model

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought,
+} = require('../../controllers/thought-controller');
+
+// Set up GET all and POST at /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+// Set up GET one, PUT, and DELETE at /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(removeThought);
+
+module.exports = router;
